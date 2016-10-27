dd7376790852b4136d7765228a0af21f24024c19
tiger154 <junhwen@naver.com>
2016-10-27 13:58:20 +0900

    package refactoring

--- src/jeonhwan/data/Main.java
+++ src/jeonhwan/data/Main.java
@@ -5,16 +5,47 @@ package jeonhwan.data;
  */
 public class Main {
 
-    public static void main(String[] args) {
+    private static void runLinkedList() {
         LinkedList numbers = new LinkedList();
         System.out.println("test start");
         // something...
         numbers.addFirstNode(30);
         numbers.addFirstNode(20);
         //numbers.addAfterNode(20,25);
+        numbers.addLastNode(40);
 
         numbers.remove(30);
 
         System.out.println("test end");
     }
+
+    private static void passByValue(){
+        class SomeNode {
+            private Object data;
+            public SomeNode(Object input) {
+                data = input;
+            }
+        }
+        SomeNode first = new SomeNode(1);
+        SomeNode second = first;
+        second.data = 2;
+        first.data = 3;
+        System.out.println("first node data :" + first.data + ", second node data : " + second.data);
+
+        int x = 1;
+        int y = x;
+        y = 2;
+        System.out.println("x value : " + x + ", y value : " + y);
+        /*  Result */
+        // first node data :3, second node data : 3
+        // x value : 1, y value : 2
+    }
+
+
+    public static void main(String[] args) {
+
+        runLinkedList();
+
+    }
+
 }
